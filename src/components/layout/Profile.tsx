830d29d746f3519d4e1b2907e8d749dd1c343450
JAngelGC <a01552274@tec.mx>
2022-12-24 01:25:15 -0600

    Linked social networks buttons

--- src/components/layout/Profile.tsx
+++ src/components/layout/Profile.tsx
@@ -2,6 +2,13 @@ import Span from "../ui/Span";
 import classes from "./Profile.module.css";
 
 const Profile: React.FC<{ classesProps: string }> = (props) => {
+  const emailToHandler: () => void = () => {
+    window.open(
+      "mailto:ja_gonzalez42@hotmail.com?subject=Subject&body=Body%20goes%20here"
+    );
+    return;
+  };
+
   return (
     <div
       className={`${classes["container-profile-ext"]} ${props.classesProps}`}
@@ -14,27 +21,33 @@ const Profile: React.FC<{ classesProps: string }> = (props) => {
         </div>
 
         <div className={classes["container-social-networks"]}>
-          <div className={classes["container-icon"]}>
-            <img
-              className={classes["icon-social-network"]}
-              src="/static/icons/email.svg"
-              alt=""
-            />
-          </div>
-          <div className={classes["container-icon"]}>
-            <img
-              className={classes["icon-social-network"]}
-              src="/static/icons/linkedin.svg"
-              alt=""
-            />
-          </div>
-          <div className={classes["container-icon"]}>
-            <img
-              className={classes["icon-social-network"]}
-              src="/static/icons/github.svg"
-              alt=""
-            />
-          </div>
+          <p className={classes["container-icon"]} onClick={emailToHandler}>
+            <div className={classes["icon-social-network"]}>
+              <img src="/static/icons/email.svg" alt="" />
+            </div>
+          </p>
+
+          <a
+            href="https://www.linkedin.com/in/j-angel-gonzalez/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes["container-icon"]}
+          >
+            <div className={classes["icon-social-network"]}>
+              <img src="/static/icons/linkedin.svg" alt="" />
+            </div>
+          </a>
+
+          <a
+            href="https://github.com/JAngelGC"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes["container-icon"]}
+          >
+            <div className={classes["icon-social-network"]}>
+              <img src="/static/icons/github.svg" alt="" />
+            </div>
+          </a>
         </div>
 
         <div className={classes["container-skills"]}>
