cb20dacd133a878e72fd9e033b3838a2b52e3552
JAngelGC <a01552274@tec.mx>
2022-12-26 19:05:40 -0600

    Updated about me information

--- src/components/about/AboutMe.tsx
+++ src/components/about/AboutMe.tsx
@@ -8,6 +8,19 @@ const AboutMe = () => {
         Tecnológico de Monterrey in Puebla, enthusiastic about how technology
         can improve all areas in our world.
       </p>
+
+      <p className={classes["text"]}>
+        I have worked with many programming languages, such as Python to create
+        an API using FastAPI or Flask, C++ to master data structures, PHP to
+        connect with SQL databases, or Elixir to create concurrent applications.
+        Currently, one of my strengths is frontend development with React.js.
+      </p>
+
+      <p className={classes["text"]}>
+        Finally, as I am in college, I am a constant team player with different
+        teams throughout the semester, where I demonstrate my skills to
+        communicate my ideas and the ability to solve any issue that presents.
+      </p>
     </section>
   );
 };
